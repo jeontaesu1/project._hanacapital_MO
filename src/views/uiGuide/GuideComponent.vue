aff928db7b844eabe48e6001770054b90fa2f5ab
dahye-mwk <da-hye.kim@mwkorea.co.kr>
2023-03-22 12:46:43 +0900

    인덱스 기능 보완 및 컴포넌트 페이지 정리 (라우터, 레이어팝업 추가)

--- src/views/uiGuide/GuideComponent.vue
+++ src/views/uiGuide/GuideComponent.vue
@@ -1,11 +1,124 @@
 <script>
+import { ref, reactive } from 'vue';
+import { RouterLink } from 'vue-router';
+
 import BasicButton from '@/components/ui/button/BasicButton.vue';
+import ButtonList from '@/components/ui/button/ButtonList.vue';
+import ButtonListItem from '@/components/ui/button/ButtonListItem.vue';
 import IconAdd from '@/assets/images/icon/add.svg?component';
+import UiLayer from '@/components/ui/layer/UiLayer.vue';
+import PopupTitle from '@/components/ui/layer/PopupTitle.vue';
+import PopupSubTitle from '@/components/ui/layer/PopupSubTitle.vue';
+import PopupText from '@/components/ui/layer/PopupText.vue';
+import PopupButton from '@/components/ui/layer/PopupButton.vue';
+import FullPopup from '@/components/ui/layer/FullPopup.vue';
+import FullPopupHead from '@/components/ui/layer/FullPopupHead.vue';
+import ToastPopup from '@/components/ui/layer/ToastPopup.vue';
+import ToastPopupHead from '@/components/ui/layer/ToastPopupHead.vue';
+import AlertPopup from '@/components/ui/layer/AlertPopup.vue';
+import AlertSystem from '@/components/ui/layer/AlertSystem.vue';
 
 export default {
   components: {
     BasicButton,
     IconAdd,
+    RouterLink,
+    UiLayer,
+    PopupTitle,
+    PopupSubTitle,
+    PopupText,
+    PopupButton,
+    FullPopup,
+    FullPopupHead,
+    ToastPopup,
+    ToastPopupHead,
+    AlertPopup,
+    AlertSystem,
+    ButtonList,
+    ButtonListItem,
+  },
+  setup() {
+    const state = reactive({
+      testError001: false,
+    });
+
+    const alert = ref(null);
+    const layerTest001 = ref(null);
+    const layerTest002 = ref(null);
+    const layerTest003 = ref(null);
+    const layerTest004 = ref(null);
+    const testInput = ref(null);
+    const testSelect001 = ref(null);
+
+    const alertOpen = (options) => {
+      alert.value.open(options);
+    };
+    const layerOpenTest001 = (e = {}) => {
+      layerTest001.value.open(e.target);
+    };
+    const layerOpenTest002 = (e = {}) => {
+      layerTest002.value.open(e.target);
+    };
+    const layerOpenTest003 = (e = {}) => {
+      layerTest003.value.open(e.target);
+    };
+    const layerOpenTest004 = (e = {}) => {
+      layerTest004.value.open(e.target);
+    };
+    const alertOpenTest001 = () => {
+      alertOpen({
+        title: '타이틀',
+        message:
+          '여기에 내용이 표시될 예정입니다.\n줄바꿈은 \\n 이용.\n세 줄일 경우는 이렿게 표시됩니다.',
+        buttons: [
+          {
+            text: '취소',
+            theme: 'quaternary',
+            line: true,
+            callback: (closeFn) => {
+              console.log('취소');
+              closeFn();
+            },
+            closeAfterCallback: () => {
+              console.log('[취소] 닫는 애니메이션 끝난 후 호출 될 함수');
+            },
+          },
+          {
+            callback: (closeFn) => {
+              console.log('확인');
+              closeFn();
+            },
+            closeAfterCallback: () => {
+              console.log('[확인] 닫는 애니메이션 끝난 후 호출 될 함수');
+            },
+          },
+        ],
+      });
+    };
+
+    const testErrorUpdate001 = (val) => {
+      state.testError001 = val;
+
+      testSelect001.value.setValue('4');
+    };
+
+    return {
+      state,
+      alert,
+      layerTest001,
+      layerTest002,
+      layerTest003,
+      layerTest004,
+      testInput,
+      testSelect001,
+      alertOpen,
+      layerOpenTest001,
+      layerOpenTest002,
+      layerOpenTest003,
+      layerOpenTest004,
+      alertOpenTest001,
+      testErrorUpdate001,
+    };
   },
 };
 </script>
@@ -98,6 +211,34 @@ export default {
           Button
         </BasicButton>
         <BasicButton tag="a" line="true" disabledStyle="true">
+          Button
+          <template v-slot:rightIcon>
+            <IconAdd />
+          </template>
+        </BasicButton>
+      </div>
+
+      <div class="test-section-sub">
+        <h3 class="test-section-sub-title">Router Link</h3>
+        <BasicButton tag="RouterLink" to="/">
+          <template v-slot:leftIcon>
+            <IconAdd />
+          </template>
+          Button
+        </BasicButton>
+        <BasicButton tag="RouterLink" to="/" disabledStyle="true">
+          Button
+          <template v-slot:rightIcon>
+            <IconAdd />
+          </template>
+        </BasicButton>
+        <BasicButton tag="RouterLink" to="/" line="true">
+          <template v-slot:leftIcon>
+            <IconAdd />
+          </template>
+          Button
+        </BasicButton>
+        <BasicButton tag="RouterLink" to="/" line="true" disabledStyle="true">
           Button
           <template v-slot:rightIcon>
             <IconAdd />
@@ -265,9 +406,180 @@ export default {
     </section>
 
     <section class="test-section">
-      <h2 class="test-section-title">Component Title</h2>
+      <h2 class="test-section-title">Layer</h2>
       <div class="test-section-sub">
-        <h3 class="test-section-sub-title">Sub Title</h3>
+        <AlertSystem ref="alert" />
+
+        <UiLayer ref="layerTest001" type="full" v-slot="layerSlotProps">
+          <FullPopup>
+            <template v-slot:head>
+              <FullPopupHead>
+                <template v-slot:left>
+                  <PopupButton type="back" />
+                  <PopupButton type="back">커스텀 텍스트</PopupButton>
+                </template>
+                <PopupTitle>타이틀</PopupTitle>
+                <template v-slot:right>
+                  <PopupButton @click="layerSlotProps.close()" />
+                </template>
+              </FullPopupHead>
+            </template>
+
+            <PopupText>// contents</PopupText>
+
+            <BasicButton @click="layerOpenTest002">
+              하단 레이어 팝업
+            </BasicButton>
+
+            <template v-slot:foot>
+              <ButtonList
+                :classNames="{
+                  wrap: 'row-margin-none',
+                }"
+              >
+                <ButtonListItem>
+                  <BasicButton :line="true" theme="quaternary"
+                    >Button 1</BasicButton
+                  >
+                </ButtonListItem>
+                <ButtonListItem>
+                  <BasicButton>Button 2</BasicButton>
+                </ButtonListItem>
+              </ButtonList>
+            </template>
+          </FullPopup>
+        </UiLayer>
+
+        <UiLayer ref="layerTest002" type="toast" v-slot="layerSlotProps">
+          <ToastPopup>
+            <template v-slot:head>
+              <ToastPopupHead>
+                <PopupTitle>타이틀</PopupTitle>
+                <PopupSubTitle>서브 타이틀 및 텍스트</PopupSubTitle>
+                <template v-slot:right>
+                  <PopupButton @click="layerSlotProps.close()" />
+                </template>
+              </ToastPopupHead>
+            </template>
+
+            <RouterLink to="a">A 페이지</RouterLink>
+            <PopupText>// contents</PopupText>
+            <PopupText>// contents</PopupText>
+            <PopupText>// contents</PopupText>
+            <PopupText>// contents</PopupText>
+            <PopupText>// contents</PopupText>
+            <PopupText>// contents</PopupText>
+
+            <BasicButton @click="layerOpenTest003">
+              하단 레이어 팝업 (고정 높이값)
+            </BasicButton>
+
+            <template v-slot:foot>
+              <ButtonList
+                :classNames="{
+                  wrap: 'row-margin-none',
+                }"
+              >
+                <ButtonListItem>
+                  <BasicButton :line="true" theme="quaternary"
+                    >Button 1</BasicButton
+                  >
+                </ButtonListItem>
+                <ButtonListItem>
+                  <BasicButton>Button 2</BasicButton>
+                </ButtonListItem>
+              </ButtonList>
+            </template>
+          </ToastPopup>
+        </UiLayer>
+
+        <UiLayer ref="layerTest003" type="toast-fix" v-slot="layerSlotProps">
+          <ToastPopup :fix="true">
+            <template v-slot:head>
+              <ToastPopupHead>
+                <PopupTitle>타이틀</PopupTitle>
+                <PopupSubTitle>서브 타이틀 및 텍스트</PopupSubTitle>
+              </ToastPopupHead>
+            </template>
+
+            <PopupText>// contents</PopupText>
+            <PopupText>// contents</PopupText>
+            <PopupText>// contents</PopupText>
+            <PopupText>// contents</PopupText>
+            <PopupText>// contents</PopupText>
+            <PopupText>// contents</PopupText>
+
+            <BasicButton @click="layerOpenTest004">
+              레이어 팝업 열기
+            </BasicButton>
+
+            <template v-slot:foot>
+              <ButtonList
+                :classNames="{
+                  wrap: 'row-margin-none',
+                }"
+              >
+                <ButtonListItem>
+                  <BasicButton
+                    :line="true"
+                    theme="quaternary"
+                    @click="layerSlotProps.close()"
+                    >Button 1</BasicButton
+                  >
+                </ButtonListItem>
+                <ButtonListItem>
+                  <BasicButton @click="layerSlotProps.close()"
+                    >Button 2</BasicButton
+                  >
+                </ButtonListItem>
+              </ButtonList>
+            </template>
+          </ToastPopup>
+        </UiLayer>
+
+        <UiLayer ref="layerTest004" v-slot="layerSlotProps">
+          <AlertPopup>
+            <template v-slot:head>
+              <PopupTitle>타이틀</PopupTitle>
+            </template>
+
+            <PopupText>// contents</PopupText>
+            <PopupText>// contents</PopupText>
+
+            <BasicButton @click="alertOpenTest001">얼럿 열기</BasicButton>
+
+            <template v-slot:foot>
+              <ButtonList
+                :classNames="{
+                  wrap: 'row-margin-none',
+                }"
+              >
+                <ButtonListItem>
+                  <BasicButton
+                    :line="true"
+                    theme="quaternary"
+                    @click="layerSlotProps.close()"
+                    >Button 1</BasicButton
+                  >
+                </ButtonListItem>
+                <ButtonListItem>
+                  <BasicButton @click="layerSlotProps.close()"
+                    >Button 2</BasicButton
+                  >
+                </ButtonListItem>
+              </ButtonList>
+            </template>
+          </AlertPopup>
+        </UiLayer>
+
+        <BasicButton @click="layerOpenTest001"> 풀 팝업 </BasicButton>
+        <BasicButton @click="layerOpenTest002"> 하단 레이어 팝업 </BasicButton>
+        <BasicButton @click="layerOpenTest003"
+          >하단 레이어 팝업 (고정 높이값)</BasicButton
+        >
+        <BasicButton @click="layerOpenTest004"> 레이어 팝업 </BasicButton>
+
+        <BasicButton @click="alertOpenTest001">얼럿</BasicButton>
       </div>
     </section>
 
