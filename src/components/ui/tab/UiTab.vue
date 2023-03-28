0d6b5bb88236363b1187fdfdd668c2ccb119bc3a
choo jeong yeon <cjy23405@mwkorea.co.kr>
2023-03-28 21:44:14 +0900

    탭 베이스 작업중

---
+++ src/components/ui/tab/UiTab.vue
@@ -0,0 +1,35 @@
+<script>
+import { computed, useCssModule, provide } from 'vue';
+
+const defaultClassNames = () => ({
+  wrap: '',
+});
+
+export default {
+  setup(props) {
+    const customClassNames = computed(() => {
+      const { classNames } = props;
+      return Object.assign(defaultClassNames(), classNames);
+    });
+
+    provide('uiTabStyleModule', useCssModule());
+    provide('uiTab', {
+      //
+    });
+
+    return {
+      customClassNames,
+    };
+  },
+};
+</script>
+
+<template>
+  <div :class="[$style['tab'], customClassNames.wrap]">
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" module>
+@import '@/assets/scss/components/ui/tab/UiTab.scss';
+</style>
---
+++ src/components/ui/tab/UiTabList.vue
@@ -0,0 +1,28 @@
+<script>
+import { computed, inject } from 'vue';
+
+const defaultClassNames = () => ({
+  item: '',
+});
+
+export default {
+  setup(props) {
+    const styleModule = inject('uiTabStyleModule');
+    const uiTab = inject('uiTab', {});
+
+    const customClassNames = computed(() => {
+      const { classNames } = props;
+      return Object.assign(defaultClassNames(), classNames);
+    });
+
+    return {
+      styleModule,
+      customClassNames,
+    };
+  },
+};
+</script>
+
+<template>
+  <div>//</div>
+</template>
