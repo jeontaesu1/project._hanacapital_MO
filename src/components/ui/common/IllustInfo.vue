61e5043ae360ad351632a313f9b51fd1a7766ceb
sjkim03 <sojin@mwkorea.co.kr>
2023-03-28 17:04:36 +0900

    Illust Info 컴포넌트 작업

---
+++ src/components/ui/common/IllustInfo.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  props: {
+    className: {
+      Type: String,
+      default: null,
+    },
+    theme: {
+      Type: String,
+      default: null,
+    },
+  },
+};
+</script>
+
+<template>
+  <div
+    :class="[
+      $style['illust-info'],
+      {
+        [$style[`illust-info--${theme}`]]: theme,
+      },
+      className,
+    ]"
+  >
+    <slot />
+  </div>
+</template>
+
+<style lang="scss" module>
+@import '@/assets/scss/components/import/IllustInfo.scss';
+</style>
---
+++ src/components/ui/text/IllustInfoTitle.vue
@@ -0,0 +1,36 @@
+<script>
+import { computed } from 'vue';
+
+const defaultClassNames = () => ({
+  wrap: '',
+});
+
+export default {
+  props: {
+    classNames: {
+      Type: Object,
+      default() {
+        return defaultClassNames();
+      },
+    },
+  },
+  setup(props) {
+    const customClassNames = computed(() => {
+      const { classNames } = props;
+      return Object.assign(defaultClassNames(), classNames);
+    });
+
+    return {
+      customClassNames,
+    };
+  },
+};
+</script>
+
+<template>
+  <p :class="[$style['illust-info__title'], customClassNames.wrap]"><slot /></p>
+</template>
+
+<style lang="scss" module>
+@import '@/assets/scss/components/ui/text/IllustInfoTitle.scss';
+</style>
