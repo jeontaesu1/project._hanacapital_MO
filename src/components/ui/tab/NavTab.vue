3323c2ac8012f27b080b0fd5177c7db87e45bb66
choo jeong yeon <cjy23405@mwkorea.co.kr>
2023-03-29 21:44:32 +0900

    nav 탭, filter 탭 컴포넌트 작업 중

---
+++ src/components/ui/tab/NavTab.vue
@@ -0,0 +1,105 @@
+<script>
+import {
+  reactive,
+  computed,
+  useCssModule,
+  provide,
+  onBeforeMount,
+  watch,
+} from 'vue';
+
+import UiTabList from '@/components/ui/tab/UiTabList.vue';
+
+const defaultClassNames = () => ({
+  wrap: '',
+  list: '',
+});
+
+export default {
+  components: {
+    UiTabList,
+  },
+  props: {
+    classNames: {
+      Type: Object,
+      default() {
+        return defaultClassNames();
+      },
+    },
+    useUiTab: {
+      Type: Boolean,
+      default: false,
+    },
+    scroll: {
+      Type: Boolean,
+      default: false,
+    },
+    auto: {
+      Type: Boolean,
+      default: false,
+    },
+  },
+  setup(props) {
+    const state = reactive({
+      useUiTab: {
+        value: null,
+      },
+    });
+
+    const customClassNames = computed(() => {
+      const { classNames } = props;
+      return Object.assign(defaultClassNames(), classNames);
+    });
+
+    const setComponent = computed(() => {
+      const { useUiTab } = props;
+      return useUiTab ? UiTabList : 'ul';
+    });
+
+    onBeforeMount(() => {
+      state.useUiTab.value = props.useUiTab;
+    });
+
+    watch(
+      () => props.useUiTab,
+      (cur) => {
+        state.useUiTab.value = cur;
+      }
+    );
+
+    provide('navTabStyleModule', useCssModule());
+    provide('navTab', {
+      useUiTab: state.useUiTab,
+    });
+
+    return {
+      customClassNames,
+      setComponent,
+    };
+  },
+};
+</script>
+
+<template>
+  <div
+    :class="[
+      $style['nav-tab'],
+      {
+        [$style['nav-tab--scroll']]: scroll,
+        [$style['nav-tab--auto']]: auto,
+      },
+      customClassNames.wrap,
+    ]"
+  >
+    <component
+      :is="setComponent"
+      :class="[$style['nav-tab__list'], customClassNames.list]"
+    >
+      <slot />
+    </component>
+  </div>
+</template>
+
+<style lang="scss" module>
+@import '@/assets/scss/components/ui/tab/NavTab.scss';
+</style>
