e02ebabc6a8ccf8a87b2646bc5eff0fd67387f45
sjkim03 <sojin@mwkorea.co.kr>
2023-05-03 09:28:14 +0900

    영업용(재고금융실사) > 재고금융실사 작업

--- src/views/InventoryFinanceCheck/InventoryFinanceCheckHome.vue
+++ src/views/InventoryFinanceCheck/InventoryFinanceCheckHome.vue
@@ -1,14 +1,85 @@
 <script>
 // IF_M11_p001
-import { onMounted, onUnmounted } from 'vue';
+import { reactive, onMounted, onUnmounted } from 'vue';
 
 import { useUiHeaderStore } from '@/stores/ui/header';
 
 import PageContents from '@/components/ui/layout/PageContents.vue';
+import BasicBox from '@/components/ui/common/BasicBox.vue';
+import BasicBoxHead from '@/components/ui/common/BasicBoxHead.vue';
+import BasicBoxHeadLeft from '@/components/ui/common/BasicBoxHeadLeft.vue';
+import KeyValue from '@/components/ui/text/KeyValue.vue';
+import KeyValueItem from '@/components/ui/text/KeyValueItem.vue';
+import KeyValueTitle from '@/components/ui/text/KeyValueTitle.vue';
+import KeyValueText from '@/components/ui/text/KeyValueText.vue';
+import ButtonList from '@/components/ui/button/ButtonList.vue';
+import ButtonListItem from '@/components/ui/button/ButtonListItem.vue';
+import BasicButton from '@/components/ui/button/BasicButton.vue';
+import TextButton from '@/components/ui/button/TextButton.vue';
+import BasicHr from '@/components/ui/common/BasicHr.vue';
+import BoxCheckList from '@/components/ui/form/BoxCheckList.vue';
+import BoxCheckListItem from '@/components/ui/form/BoxCheckListItem.vue';
+import BoxCheck from '@/components/ui/form/BoxCheck.vue';
+import BoxCheckLabel from '@/components/ui/form/BoxCheckLabel.vue';
+import InputBlock from '@/components/ui/form/InputBlock.vue';
+import InputBlockCell from '@/components/ui/form/InputBlockCell.vue';
+import BasicInput from '@/components/ui/form/BasicInput.vue';
+import BasicSelect from '@/components/ui/form/BasicSelect.vue';
+import FormList from '@/components/ui/form/FormList.vue';
+import FormListItem from '@/components/ui/form/FormListItem.vue';
+import FormInvalid from '@/components/ui/form/FormInvalid.vue';
+import FormInvalidMessage from '@/components/ui/form/FormInvalidMessage.vue';
+
+import IconArrow from '@/assets/images/icon/dropdown.svg?component';
+
+const dummyData = () => [
+  {
+    number: '180주8300',
+    name: 'K5 하이브리드(DL3) 노블레스',
+    date: '2022.09.22',
+    branch001: '퍼펌승',
+    branch002: '퍼펌승',
+    deadline: '2022.10.06',
+  },
+  {
+    number: '180주8300',
+    name: 'K5 하이브리드(DL3) 노블레스',
+    date: '2022.09.22',
+    branch001: '퍼펌승',
+    branch002: '퍼펌승',
+    deadline: '2022.10.06',
+  },
+];
 
 export default {
   components: {
     PageContents,
+    BasicBox,
+    BasicBoxHead,
+    BasicBoxHeadLeft,
+    KeyValue,
+    KeyValueItem,
+    KeyValueTitle,
+    KeyValueText,
+    ButtonList,
+    ButtonListItem,
+    BasicButton,
+    TextButton,
+    BasicHr,
+    BoxCheckList,
+    BoxCheckListItem,
+    BoxCheck,
+    BoxCheckLabel,
+    InputBlock,
+    InputBlockCell,
+    BasicInput,
+    BasicSelect,
+    FormList,
+    FormListItem,
+    FormInvalid,
+    FormInvalidMessage,
+
+    IconArrow,
   },
   setup() {
     const store = {
@@ -17,10 +88,20 @@ export default {
       },
     };
 
+    const state = reactive({
+      departmentError: false,
+      branchError001: false,
+      branchError002: false,
+      searchError001: false,
+      searchError002: false,
+
+      data: dummyData(),
+    });
+
     onMounted(() => {
-      store.ui.header.setTitle(() => '타이틀');
+      store.ui.header.setTitle(() => '재고금융실사');
       store.ui.header.setLeftButtons(() => ['back']);
-      store.ui.header.setRightButtons(() => []);
+      store.ui.header.setRightButtons(() => ['menu']);
     });
 
     onUnmounted(() => {
@@ -28,13 +109,274 @@ export default {
       store.ui.header.setLeftButtons();
       store.ui.header.setRightButtons();
     });
+
+    return {
+      state,
+    };
   },
 };
 </script>
 
 <template>
   <PageContents>
-    <h1>Page</h1>
+    <BasicBox className="row-margin-contents-group">
+      <div class="flex-box">
+        <div class="flex-box__cell flex-1">
+          <p class="text-body-3">간편한 등록을 원하신다면?</p>
+        </div>
+        <div class="flex-box__cell flex-box__cell--medium">
+          <BasicButton size="mini">실사촬영</BasicButton>
+        </div>
+      </div>
+    </BasicBox>
+
+    <section>
+      <FormList>
+        <!-- DD : 계정의 취급부서 자동 입력 -->
+        <FormListItem
+          titleText="취급부서"
+          target="#inventoryFinanceCheckHomeDepartment"
+        >
+          <FormInvalid :error="state.departmentError">
+            <InputBlock :error="state.departmentError">
+              <InputBlockCell :flexible="true">
+                <BasicInput
+                  title="취급부서"
+                  defaultValue="재고관리부"
+                  :disabled="true"
+                  id="inventoryFinanceCheckHomeDepartment"
+                />
+              </InputBlockCell>
+            </InputBlock>
+            <FormInvalidMessage>Error Message</FormInvalidMessage>
+          </FormInvalid>
+        </FormListItem>
+        <!-- // DD : 계정의 취급부서 자동 입력 -->
+
+        <FormListItem
+          titleText="제휴점"
+          target="#inventoryFinanceCheckHomeBranch001Search"
+        >
+          <FormInvalid :error="state.branchError001">
+            <InputBlock :error="state.branchError001">
+              <InputBlockCell :flexible="true">
+                <BasicInput
+                  title="제휴점"
+                  defaultValue="현대자동차매매상사"
+                  :disabled="true"
+                  id="inventoryFinanceCheckHomeBranch001"
+                />
+              </InputBlockCell>
+              <template v-slot:right>
+                <BasicButton
+                  size="mini"
+                  theme="tertiary"
+                  id="inventoryFinanceCheckHomeBranch001Search"
+                  >검색</BasicButton
+                >
+              </template>
+            </InputBlock>
+            <FormInvalidMessage>Error Message</FormInvalidMessage>
+          </FormInvalid>
+        </FormListItem>
+
+        <FormListItem
+          titleText="매매상사"
+          target="#inventoryFinanceCheckHomeBranchButton002"
+          :selectOnly="true"
+        >
+          <FormInvalid :error="state.branchError002">
+            <InputBlock :error="state.branchError002">
+              <InputBlockCell :flexible="true">
+                <BasicSelect
+                  :option="[
+                    {
+                      value: '1',
+                      text: '전체',
+                    },
+                    {
+                      value: '2',
+                      text: '$제휴점 > 매매상사명$',
+                    },
+                  ]"
+                  buttonTitle="매매상사 선택하기"
+                  layerTitle="매매상사를 선택해 주세요"
+                  id="inventoryFinanceCheckHomeBranch002"
+                  buttonId="inventoryFinanceCheckHomeBranchButton002"
+                />
+              </InputBlockCell>
+            </InputBlock>
+            <FormInvalidMessage>Error Message</FormInvalidMessage>
+          </FormInvalid>
+        </FormListItem>
+
+        <FormListItem titleText="검색조건" :forceFocus="true">
+          <FormInvalid :error="state.searchError001">
+            <BoxCheckList>
+              <BoxCheckListItem>
+                <BoxCheck
+                  :minSide="true"
+                  name="inventoryFinanceCheckHomeSearch001"
+                  id="inventoryFinanceCheckHomeSearch001_001"
+                  :defaultChecked="true"
+                >
+                  <BoxCheckLabel>차량번호</BoxCheckLabel>
+                </BoxCheck>
+              </BoxCheckListItem>
+              <BoxCheckListItem>
+                <BoxCheck
+                  :minSide="true"
+                  name="inventoryFinanceCheckHomeSearch001"
+                  id="inventoryFinanceCheckHomeSearch001_002"
+                >
+                  <BoxCheckLabel>채권번호</BoxCheckLabel>
+                </BoxCheck>
+              </BoxCheckListItem>
+            </BoxCheckList>
+            <FormInvalidMessage>Error Message</FormInvalidMessage>
+          </FormInvalid>
+        </FormListItem>
+
+        <FormListItem
+          titleText="검색어"
+          target="#inventoryFinanceCheckHomeSearch002"
+        >
+          <FormInvalid :error="state.searchError002">
+            <InputBlock :error="state.searchError002">
+              <InputBlockCell :flexible="true">
+                <BasicInput
+                  title="검색어"
+                  id="inventoryFinanceCheckHomeSearch002"
+                />
+              </InputBlockCell>
+            </InputBlock>
+            <FormInvalidMessage>Error Message</FormInvalidMessage>
+          </FormInvalid>
+        </FormListItem>
+      </FormList>
+
+      <div class="row-margin-contents-group">
+        <ButtonList
+          :classNames="{
+            wrap: 'row-margin-none',
+          }"
+        >
+          <ButtonListItem>
+            <BasicButton :line="true" theme="quaternary">초기화</BasicButton>
+          </ButtonListItem>
+          <ButtonListItem>
+            <BasicButton :line="true">검색</BasicButton>
+          </ButtonListItem>
+        </ButtonList>
+      </div>
+    </section>
+
+    <!-- Case : 검색 후 노출 -->
+    <BasicHr className="row-margin-container-medium" />
+
+    <section class="contents-wrap">
+      <!-- Case : 검색내역이 없을 경우 -->
+      <div :class="$style['empty']">
+        <p :class="$style['empty__text']">검색된 내역이 없습니다.</p>
+      </div>
+      <!-- //Case : 검색내역이 없을 경우 -->
+
+      <!-- Case : 검색내역 있을 경우 -->
+      <ul class="reset-list row-margin-item-group">
+        <li
+          v-for="(item, i) in state.data"
+          :key="i"
+          class="row-margin-item-group"
+        >
+          <BasicBox>
+            <BasicBoxHead>
+              <BasicBoxHeadLeft>
+                <h3 class="text-body-1 font-weight-medium">
+                  {{ item.number }}
+                </h3>
+                <p class="text-body-4 color-gray row-margin-small">
+                  {{ item.name }}
+                </p>
+              </BasicBoxHeadLeft>
+            </BasicBoxHead>
+
+            <KeyValue margin="regular">
+              <KeyValueItem
+                :classNames="{
+                  item: 'text-body-3',
+                }"
+              >
+                <KeyValueTitle>실행일</KeyValueTitle>
+                <KeyValueText>{{ item.date }}</KeyValueText>
+              </KeyValueItem>
+
+              <KeyValueItem
+                :classNames="{
+                  item: 'text-body-3',
+                }"
+              >
+                <KeyValueTitle>제휴점</KeyValueTitle>
+                <KeyValueText>{{ item.branch001 }}</KeyValueText>
+              </KeyValueItem>
+
+              <KeyValueItem
+                :classNames="{
+                  item: 'text-body-3',
+                }"
+              >
+                <KeyValueTitle>매매상사</KeyValueTitle>
+                <KeyValueText>{{ item.branch002 }}</KeyValueText>
+              </KeyValueItem>
+
+              <KeyValueItem
+                :classNames="{
+                  item: 'text-body-3',
+                }"
+              >
+                <KeyValueTitle>실사기한</KeyValueTitle>
+                <KeyValueText>{{ item.deadline }}</KeyValueText>
+              </KeyValueItem>
+            </KeyValue>
+
+            <div class="row-margin-contents-small">
+              <ButtonList>
+                <ButtonListItem>
+                  <BasicButton :line="true" size="small"
+                    >미실사 사유등록</BasicButton
+                  >
+                </ButtonListItem>
+                <ButtonListItem>
+                  <BasicButton :line="true" size="small">실사등록</BasicButton>
+                </ButtonListItem>
+              </ButtonList>
+              <ButtonList
+                :classNames="{
+                  wrap: 'row-margin-item',
+                }"
+              >
+                <ButtonListItem>
+                  <BasicButton size="small">신청하기</BasicButton>
+                </ButtonListItem>
+              </ButtonList>
+            </div>
+          </BasicBox>
+        </li>
+      </ul>
+
+      <div class="inline-wrap align-center row-margin-item-group">
+        <TextButton :classNames="{ wrap: 'text-body-4 color-gray' }">
+          더보기
+          <template v-slot:rightIcon>
+            <IconArrow />
+          </template>
+        </TextButton>
+      </div>
+      <!-- //Case : 검색내역 있을 경우 -->
+    </section>
+    <!-- //Case : 검색 후 노출 -->
   </PageContents>
 </template>
 
+<style lang="scss" module>
+@import '@/assets/scss/views/inventoryFinanceCheck/InventoryFinanceCheckHome.scss';
+</style>
--- src/views/InventoryFinanceCheck/LayerInventoryFinanceCheckForm001.vue
+++ src/views/InventoryFinanceCheck/LayerInventoryFinanceCheckForm001.vue
@@ -1,3 +1,92 @@
 <script>
 // IF_M11_l001
+import { ref, reactive } from 'vue';
+
+import UiLayer from '@/components/ui/layer/UiLayer.vue';
+import PopupButton from '@/components/ui/layer/PopupButton.vue';
+import FullPopup from '@/components/ui/layer/FullPopup.vue';
+import FullPopupHead from '@/components/ui/layer/FullPopupHead.vue';
+import PopupTitle from '@/components/ui/layer/PopupTitle.vue';
+import ButtonList from '@/components/ui/button/ButtonList.vue';
+import ButtonListItem from '@/components/ui/button/ButtonListItem.vue';
+import BasicButton from '@/components/ui/button/BasicButton.vue';
+import BasicBox from '@/components/ui/common/BasicBox.vue';
+import FormList from '@/components/ui/form/FormList.vue';
+import FormInvalidMessage from '@/components/ui/form/FormInvalidMessage.vue';
+import BasicTextarea from '@/components/ui/form/BasicTextarea.vue';
+
+export default {
+  components: {
+    UiLayer,
+    PopupButton,
+    FullPopup,
+    FullPopupHead,
+    PopupTitle,
+    ButtonList,
+    ButtonListItem,
+    BasicButton,
+    BasicBox,
+    FormList,
+    FormInvalidMessage,
+    BasicTextarea,
+  },
+  setup() {
+    const state = reactive({
+      reasonError: false,
+    });
+
+    const layer = ref(null);
+
+    return {
+      state,
+      layer,
+    };
+  },
+};
 </script>
+
+<template>
+  <UiLayer ref="layer" type="full" v-slot="layerSlotProps">
+    <FullPopup>
+      <template v-slot:head>
+        <FullPopupHead>
+          <PopupTitle>미실사정보입력</PopupTitle>
+          <template v-slot:right>
+            <PopupButton @click="layerSlotProps.close()" />
+          </template>
+        </FullPopupHead>
+      </template>
+
+      <BasicBox theme="senary">
+        <h3 class="text-body-1 font-weight-medium">180주8300</h3>
+        <p class="text-body-4 color-gray row-margin-small">
+          K5 하이브리드(DL3) 노블레스
+        </p>
+      </BasicBox>
+
+      <FormList :classNames="{ wrap: 'row-margin-contents-group' }">
+        <BasicTextarea
+          :error="state.reasonError"
+          titleText="미실사 사유"
+          title="미실사 사유"
+        >
+          <template v-slot:bottom>
+            <FormInvalidMessage>Error Message</FormInvalidMessage>
+          </template>
+        </BasicTextarea>
+      </FormList>
+
+      <template v-slot:foot>
+        <ButtonList
+          :classNames="{
+            wrap: 'row-margin-none',
+          }"
+        >
+          <ButtonListItem>
+            <BasicButton>저장</BasicButton>
+          </ButtonListItem>
+        </ButtonList>
+      </template>
+    </FullPopup>
+  </UiLayer>
+</template>
