996585d4ea12339b3066ccc077dc5eabf26d3c7d
kim tae kyun <nonoday@mwkorea.co.kr>
2023-05-03 19:14:46 +0900

    임시커밋

--- src/views/uiGuide/GuideSalesUsedCar001.vue
+++ src/views/uiGuide/GuideSalesUsedCar001.vue
@@ -6,7 +6,11 @@ import BasicButton from '@/components/ui/button/BasicButton.vue';
 import ButtonList from '@/components/ui/button/ButtonList.vue';
 import ButtonListItem from '@/components/ui/button/ButtonListItem.vue';
 
-// import Layer from '@/views/';
+import LayerSalesUsedCarStateList from '@/views/salesUsedCar/LayerSalesUsedCarStateList.vue';
+import LayerSalesUsedCarCounselingConfirm from '@/views/salesUsedCar/LayerSalesUsedCarCounselingConfirm.vue';
+import LayerSalesUsedCarCounselingCommission from '@/views/salesUsedCar/LayerSalesUsedCarCounselingCommission.vue';
+import LayerSalesUsedCarCounselingAccount from '@/views/salesUsedCar/LayerSalesUsedCarCounselingAccount.vue';
+import LayerSalesUsedCarCounselingAccountSelect from '@/views/salesUsedCar/LayerSalesUsedCarCounselingAccountSelect.vue';
 
 export default {
   components: {
@@ -14,18 +18,46 @@ export default {
     BasicButton,
     ButtonList,
     ButtonListItem,
-    // Layer,
+    LayerSalesUsedCarStateList,
+    LayerSalesUsedCarCounselingConfirm,
+    LayerSalesUsedCarCounselingCommission,
+    LayerSalesUsedCarCounselingAccount,
+    LayerSalesUsedCarCounselingAccountSelect,
   },
   setup() {
     const layer001 = ref(null);
+    const layer002 = ref(null);
+    const layer003 = ref(null);
+    const layer004 = ref(null);
+    const layer005 = ref(null);
 
     const layer001Open = (e = {}) => {
       layer001.value.layer.open(e.target);
     };
+    const layer002Open = (e = {}) => {
+      layer002.value.layer.open(e.target);
+    };
+    const layer003Open = (e = {}) => {
+      layer003.value.layer.open(e.target);
+    };
+    const layer004Open = (e = {}) => {
+      layer004.value.layer.open(e.target);
+    };
+    const layer005Open = (e = {}) => {
+      layer005.value.layer.open(e.target);
+    };
 
     return {
       layer001,
+      layer002,
+      layer003,
+      layer004,
+      layer005,
       layer001Open,
+      layer002Open,
+      layer003Open,
+      layer004Open,
+      layer005Open,
     };
   },
 };
@@ -41,11 +73,35 @@ export default {
     >
       <ButtonListItem>
         <BasicButton @click="layer001Open"
-          >ㅇㅇㅇㅇㅇ<br />ㅁㅁㅁㅁㅁㅁ</BasicButton
-        >
+          >매매상사 약정 현황 조회<br />UC_M01_l001
+        </BasicButton>
+      </ButtonListItem>
+      <ButtonListItem>
+        <BasicButton @click="layer002Open"
+          >차량정보 팝업<br />UC_M03_l001
+        </BasicButton>
+      </ButtonListItem>
+      <ButtonListItem>
+        <BasicButton @click="layer003Open"
+          >수수료차감 팝업<br />UC_M03_l002
+        </BasicButton>
+      </ButtonListItem>
+      <ButtonListItem>
+        <BasicButton @click="layer004Open"
+          >매매상사(차량 소유자)계좌 조회<br />UC_M03_l003
+        </BasicButton>
+      </ButtonListItem>
+      <ButtonListItem>
+        <BasicButton @click="layer005Open"
+          >매매상사(차량 소유자)계좌 등록<br />UC_M03_l004
+        </BasicButton>
       </ButtonListItem>
     </ButtonList>
 
-    <Layer ref="layer001" />
+    <LayerSalesUsedCarStateList ref="layer001" />
+    <LayerSalesUsedCarCounselingConfirm ref="layer002" />
+    <LayerSalesUsedCarCounselingCommission ref="layer003" />
+    <LayerSalesUsedCarCounselingAccount ref="layer004" />
+    <LayerSalesUsedCarCounselingAccountSelect ref="layer005" />
   </PageContents>
 </template>
