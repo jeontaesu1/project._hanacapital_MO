629370b0d82d5386889c48be0d7479c9f9f4ef93
cheeze <cjy23405@gmail.com>
2023-05-07 01:02:07 +0900

    KSJ 미완 분량

--- src/views/myInfo/MyInfoMarketing.vue
+++ src/views/myInfo/MyInfoMarketing.vue
@@ -5,10 +5,28 @@ import { onMounted, onUnmounted } from 'vue';
 import { useUiHeaderStore } from '@/stores/ui/header';
 
 import PageContents from '@/components/ui/layout/PageContents.vue';
+import BasicBox from '@/components/ui/common/BasicBox.vue';
+import BasicBoxHead from '@/components/ui/common/BasicBoxHead.vue';
+import BasicBoxHeadLeft from '@/components/ui/common/BasicBoxHeadLeft.vue';
+import CheckBox from '@/components/ui/form/CheckBox.vue';
+import CheckBoxLabelText from '@/components/ui/form/CheckBoxLabelText.vue';
+import CheckBoxObject from '@/components/ui/form/CheckBoxObject.vue';
+import BasicButton from '@/components/ui/button/BasicButton.vue';
+import ButtonList from '@/components/ui/button/ButtonList.vue';
+import ButtonListItem from '@/components/ui/button/ButtonListItem.vue';
 
 export default {
   components: {
     PageContents,
+    BasicBox,
+    BasicBoxHead,
+    BasicBoxHeadLeft,
+    CheckBox,
+    CheckBoxLabelText,
+    CheckBoxObject,
+    BasicButton,
+    ButtonList,
+    ButtonListItem,
   },
   setup() {
     const store = {
@@ -18,7 +36,7 @@ export default {
     };
 
     onMounted(() => {
-      store.ui.header.setTitle(() => '타이틀');
+      store.ui.header.setTitle(() => '마케팅 수신 동의 변경');
       store.ui.header.setLeftButtons(() => ['back']);
       store.ui.header.setRightButtons(() => []);
     });
@@ -34,6 +52,192 @@ export default {
 
 <template>
   <PageContents>
-    <h1>Page</h1>
+    <div>
+      <section class="row-margin-container-medium">
+        <div class="row-margin-contents">
+          <h3 class="text-title-2">하나캐피탈 마케팅 수신 동의</h3>
+          <p
+            class="text-body-3 font-weight-light color-gray-tertiary row-margin-small"
+          >
+            하나캐피탈에서 발송하는 마케팅을 위한 전화, 문자, 메시지, 우편,
+            이메일에 대한 수신동의를 설정하실 수 있습니다.
+          </p>
+        </div>
+
+        <BasicBox>
+          <BasicBoxHead align="top">
+            <BasicBoxHeadLeft>
+              <CheckBox
+                id="myInfoMarketingAgree001All"
+                :classNames="{ wrap: $style['agree-list__all-checkbox'] }"
+              >
+                <CheckBoxObject />
+                <CheckBoxLabelText>전체 수신</CheckBoxLabelText>
+              </CheckBox>
+            </BasicBoxHeadLeft>
+          </BasicBoxHead>
+
+          <div
+            :class="[$style['check-box-list'], $style['check-box-list--wrap']]"
+          >
+            <ul :class="$style['check-box-list__list']">
+              <li :class="$style['check-box-list__item']">
+                <CheckBox
+                  id="myInfoMarketingAgree001_001"
+                  :classNames="{
+                    wrap: $style['agree-list__checkbox'],
+                  }"
+                  theme="quinary"
+                >
+                  <CheckBoxObject />
+                  <CheckBoxLabelText>전화</CheckBoxLabelText>
+                </CheckBox>
+              </li>
+              <li :class="$style['check-box-list__item']">
+                <CheckBox
+                  id="myInfoMarketingAgree001_002"
+                  :classNames="{
+                    wrap: $style['agree-list__checkbox'],
+                  }"
+                  theme="quinary"
+                >
+                  <CheckBoxObject />
+                  <CheckBoxLabelText>문자메시지</CheckBoxLabelText>
+                </CheckBox>
+              </li>
+              <li :class="$style['check-box-list__item']">
+                <CheckBox
+                  id="myInfoMarketingAgree001_003"
+                  :classNames="{
+                    wrap: $style['agree-list__checkbox'],
+                  }"
+                  theme="quinary"
+                >
+                  <CheckBoxObject />
+                  <CheckBoxLabelText>우편</CheckBoxLabelText>
+                </CheckBox>
+              </li>
+              <li :class="$style['check-box-list__item']">
+                <CheckBox
+                  id="myInfoMarketingAgree001_004"
+                  :classNames="{
+                    wrap: $style['agree-list__checkbox'],
+                  }"
+                  theme="quinary"
+                >
+                  <CheckBoxObject />
+                  <CheckBoxLabelText>이메일</CheckBoxLabelText>
+                </CheckBox>
+              </li>
+            </ul>
+          </div>
+
+          <ButtonList
+            :classNames="{
+              wrap: 'row-margin-contents-small',
+            }"
+          >
+            <ButtonListItem>
+              <BasicButton size="small">확인</BasicButton>
+            </ButtonListItem>
+          </ButtonList>
+        </BasicBox>
+      </section>
+
+      <section class="row-margin-container-medium">
+        <div class="row-margin-contents">
+          <h3 class="text-title-2">하나캐피탈 멤버십 수신 동의</h3>
+          <p
+            class="text-body-3 font-weight-light color-gray-tertiary row-margin-small"
+          >
+            하나캐피탈 멤버십에서 발송하는 마케팅을 위한 전화, 문자메시지,
+            이메일, App Push에 대한 수신동의를 설정할 수 있습니다.
+          </p>
+        </div>
+
+        <BasicBox>
+          <BasicBoxHead align="top">
+            <BasicBoxHeadLeft>
+              <CheckBox
+                id="myInfoMarketingAgree002All"
+                :classNames="{ wrap: $style['agree-list__all-checkbox'] }"
+              >
+                <CheckBoxObject />
+                <CheckBoxLabelText>전체 수신</CheckBoxLabelText>
+              </CheckBox>
+            </BasicBoxHeadLeft>
+          </BasicBoxHead>
+
+          <div
+            :class="[$style['check-box-list'], $style['check-box-list--wrap']]"
+          >
+            <ul :class="$style['check-box-list__list']">
+              <li :class="$style['check-box-list__item']">
+                <CheckBox
+                  id="myInfoMarketingAgree002_001"
+                  :classNames="{
+                    wrap: $style['agree-list__checkbox'],
+                  }"
+                  theme="quinary"
+                >
+                  <CheckBoxObject />
+                  <CheckBoxLabelText>전화</CheckBoxLabelText>
+                </CheckBox>
+              </li>
+              <li :class="$style['check-box-list__item']">
+                <CheckBox
+                  id="myInfoMarketingAgree002_002"
+                  :classNames="{
+                    wrap: $style['agree-list__checkbox'],
+                  }"
+                  theme="quinary"
+                >
+                  <CheckBoxObject />
+                  <CheckBoxLabelText>문자메시지</CheckBoxLabelText>
+                </CheckBox>
+              </li>
+              <li :class="$style['check-box-list__item']">
+                <CheckBox
+                  id="myInfoMarketingAgree002_003"
+                  :classNames="{
+                    wrap: $style['agree-list__checkbox'],
+                  }"
+                  theme="quinary"
+                >
+                  <CheckBoxObject />
+                  <CheckBoxLabelText>우편</CheckBoxLabelText>
+                </CheckBox>
+              </li>
+              <li :class="$style['check-box-list__item']">
+                <CheckBox
+                  id="myInfoMarketingAgree002_004"
+                  :classNames="{
+                    wrap: $style['agree-list__checkbox'],
+                  }"
+                  theme="quinary"
+                >
+                  <CheckBoxObject />
+                  <CheckBoxLabelText>이메일</CheckBoxLabelText>
+                </CheckBox>
+              </li>
+            </ul>
+          </div>
+
+          <ButtonList
+            :classNames="{
+              wrap: 'row-margin-contents-small',
+            }"
+          >
+            <ButtonListItem>
+              <BasicButton size="small">확인</BasicButton>
+            </ButtonListItem>
+          </ButtonList>
+        </BasicBox>
+      </section>
+    </div>
   </PageContents>
 </template>
+
+<style lang="scss" module>
+@import '@/assets/scss/views/myInfo/MyInfoMarketing.scss';
+</style>
