006ae7fb6db01e103db8b64a64fc6af577bdc929
kim tae kyun <nonoday@mwkorea.co.kr>
2023-05-11 17:49:46 +0900

    복사용 커밋

--- src/views/customer/CustomerClause.vue
+++ src/views/customer/CustomerClause.vue
@@ -27,7 +27,7 @@ export default {
     onMounted(() => {
       store.ui.header.setTitle(() => '정책 및 약관');
       store.ui.header.setLeftButtons(() => ['back']);
-      store.ui.header.setRightButtons(() => ['menu']);
+      store.ui.header.setRightButtons(() => []);
     });
 
     onUnmounted(() => {
