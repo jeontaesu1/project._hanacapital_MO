618a0747d7bce6117d3d133363116aa06ddf1e72
sjkim03 <sojin@mwkorea.co.kr>
2023-05-18 21:45:54 +0900

    개인금융 > 통합 한도조회 > 한도조회	한도조회 입력 > 아파트 등기부등본 확인 팝업

--- src/views/uiGuide/GuidePersonalLoanETC001.vue
+++ src/views/uiGuide/GuidePersonalLoanETC001.vue
@@ -12,6 +12,7 @@ import LayerPersonalLoanETCAutoSubmit from '@/views/personalLoan/LayerPersonalLo
 import LayerPersonalLoanETCValusation from '@/views/personalLoan/LayerPersonalLoanETCValusation.vue';
 import LayerPersonalLoanETCCheck from '@/views/personalLoan/LayerPersonalLoanETCCheck.vue';
 import LayerPersonalLoanETCSearchApartment from '@/views/personalLoan/LayerPersonalLoanETCSearchApartment.vue';
+import LayerPersonalLoanETCApartmentCertifiedConfirm from '@/views/personalLoan/LayerPersonalLoanETCApartmentCertifiedConfirm.vue';
 
 export default {
   components: {
@@ -25,6 +26,7 @@ export default {
     LayerPersonalLoanETCValusation,
     LayerPersonalLoanETCCheck,
     LayerPersonalLoanETCSearchApartment,
+    LayerPersonalLoanETCApartmentCertifiedConfirm,
   },
   setup() {
     const layer001 = ref(null);
@@ -33,6 +35,7 @@ export default {
     const layer004 = ref(null);
     const layer005 = ref(null);
     const layer006 = ref(null);
+    const layer007 = ref(null);
 
     const layer001Open = (e = {}) => {
       layer001.value.layer.open(e.target);
@@ -52,6 +55,9 @@ export default {
     const layer006Open = (e = {}) => {
       layer006.value.layer.open(e.target);
     };
+    const layer007Open = (e = {}) => {
+      layer007.value.layer.open(e.target);
+    };
 
     return {
       layer001,
@@ -60,12 +66,14 @@ export default {
       layer004,
       layer005,
       layer006,
+      layer007,
       layer001Open,
       layer002Open,
       layer003Open,
       layer004Open,
       layer005Open,
       layer006Open,
+      layer007Open,
     };
   },
 };
@@ -114,6 +122,12 @@ export default {
           한도/금리 확인<br />PF_M11_l007
         </BasicButton>
       </ButtonListItem>
+
+      <ButtonListItem>
+        <BasicButton @click="layer007Open">
+          아파트 등기부등본 확인<br />PF_M11_l006
+        </BasicButton>
+      </ButtonListItem>
     </ButtonList>
 
     <LayerPersonalLoanETCAgree ref="layer001" />
@@ -122,5 +136,6 @@ export default {
     <LayerPersonalLoanETCSearchApartment ref="layer006" />
     <LayerPersonalLoanETCValusation ref="layer004" />
     <LayerPersonalLoanETCCheck ref="layer005" />
+    <LayerPersonalLoanETCApartmentCertifiedConfirm ref="layer007" />
   </PageContents>
 </template>
