145da1bd2716ae5d6fe0205c259f3f79e9e57001
dahye-mwk <da-hye.kim@mwkorea.co.kr>
2023-05-31 22:24:33 +0900

    영업용 (리스/렌트 견적시스템) > 저장.심사 작업

--- src/views/LeaseRentEstimationSystem/LayerLeaseRentEstimationSystemSaveReviewCarNumber.vue
+++ src/views/LeaseRentEstimationSystem/LayerLeaseRentEstimationSystemSaveReviewCarNumber.vue
@@ -1,31 +1,52 @@
 <script>
 // LR_M04_b001
-import { ref } from 'vue';
+import { ref, reactive } from 'vue';
 
 import UiLayer from '@/components/ui/layer/UiLayer.vue';
-import PopupTitle from '@/components/ui/layer/PopupTitle.vue';
-import PopupButton from '@/components/ui/layer/PopupButton.vue';
 import FullPopup from '@/components/ui/layer/FullPopup.vue';
 import FullPopupHead from '@/components/ui/layer/FullPopupHead.vue';
+import PopupButton from '@/components/ui/layer/PopupButton.vue';
+import PageTextGroup from '@/components/ui/text/PageTextGroup.vue';
+import PageMainText from '@/components/ui/text/PageMainText.vue';
+import FormList from '@/components/ui/form/FormList.vue';
+import FormListItem from '@/components/ui/form/FormListItem.vue';
+import FormInvalid from '@/components/ui/form/FormInvalid.vue';
+import InputBlock from '@/components/ui/form/InputBlock.vue';
+import InputBlockCell from '@/components/ui/form/InputBlockCell.vue';
+import BasicInput from '@/components/ui/form/BasicInput.vue';
 import BasicButton from '@/components/ui/button/BasicButton.vue';
+import FormInvalidMessage from '@/components/ui/form/FormInvalidMessage.vue';
 import ButtonList from '@/components/ui/button/ButtonList.vue';
 import ButtonListItem from '@/components/ui/button/ButtonListItem.vue';
 
 export default {
   components: {
     UiLayer,
-    PopupTitle,
-    PopupButton,
     FullPopup,
     FullPopupHead,
+    PopupButton,
+    PageTextGroup,
+    PageMainText,
+    FormList,
+    FormListItem,
+    FormInvalid,
+    InputBlock,
+    InputBlockCell,
+    BasicInput,
     BasicButton,
+    FormInvalidMessage,
     ButtonList,
     ButtonListItem,
   },
   setup() {
+    const state = reactive({
+      carNumberError: false,
+    });
+
     const layer = ref(null);
 
     return {
+      state,
       layer,
     };
   },
@@ -37,14 +58,53 @@ export default {
     <FullPopup>
       <template v-slot:head>
         <FullPopupHead>
-          <PopupTitle>타이틀</PopupTitle>
           <template v-slot:right>
             <PopupButton @click="layerSlotProps.close()" />
           </template>
         </FullPopupHead>
       </template>
 
-      // contents
+      <PageTextGroup>
+        <PageMainText>
+          차량번호를<br />
+          <strong>등록해 주세요</strong>
+        </PageMainText>
+      </PageTextGroup>
+
+      <div>
+        <FormList>
+          <FormListItem
+            titleText="차량번호"
+            target="#layerLeaseRentEstimationSystemSaveReviewCarNumberCarNumberInput"
+          >
+            <FormInvalid :error="state.carNumberError">
+              <InputBlock :error="state.carNumberError">
+                <InputBlockCell :flexible="true">
+                  <BasicInput
+                    title="차량번호"
+                    id="layerLeaseRentEstimationSystemSaveReviewCarNumberCarNumberInput"
+                  />
+                </InputBlockCell>
+                <template v-slot:right>
+                  <BasicButton size="mini" theme="tertiary">확인</BasicButton>
+                </template>
+              </InputBlock>
+
+              <FormInvalidMessage>Error Message</FormInvalidMessage>
+            </FormInvalid>
+          </FormListItem>
+        </FormList>
+
+        <ul :class="[$style['basic-list'], 'row-margin-contents']">
+          <li :class="$style['basic-list__item']">
+            <div :class="$style['basic-list__symbol']"></div>
+            <div :class="$style['basic-list__content']">
+              기타문의사항은 하나캐피탈 고객센터(1800-1110)으로 연락주시기
+              바랍니다.
+            </div>
+          </li>
+        </ul>
+      </div>
 
       <template v-slot:foot>
         <ButtonList
@@ -53,13 +113,14 @@ export default {
           }"
         >
           <ButtonListItem>
-            <BasicButton :line="true" theme="quaternary">버튼</BasicButton>
-          </ButtonListItem>
-          <ButtonListItem>
-            <BasicButton>버튼</BasicButton>
+            <BasicButton>확인</BasicButton>
           </ButtonListItem>
         </ButtonList>
       </template>
     </FullPopup>
   </UiLayer>
 </template>
+
+<style lang="scss" module>
+@import '@/assets/scss/views/LeaseRentEstimationSystem/LayerLeaseRentEstimationSystemSaveReviewCarNumber.scss';
+</style>
