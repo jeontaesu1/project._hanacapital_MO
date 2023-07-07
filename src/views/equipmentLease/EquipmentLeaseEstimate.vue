11e2a2ada28a61db91cc231f7c60c8ee21dac21c
cheeze <cjy23405@gmail.com>
2023-07-07 11:32:56 +0900

    EL_M01_p001 적용금리 항목 인풋으로 변경

--- src/views/equipmentLease/EquipmentLeaseEstimate.vue
+++ src/views/equipmentLease/EquipmentLeaseEstimate.vue
@@ -79,6 +79,7 @@ export default {
       nameError: false,
       equipmentNameError: false,
       acquisitionCostError: false,
+      interestRateError: false,
       depositRatioError: false,
       depositInputError: false,
       valueRatioError: false,
@@ -511,7 +512,35 @@ export default {
                     <KeyValueTitle>
                       <div class="text-body-4">적용금리</div>
                     </KeyValueTitle>
-                    <KeyValueText>0%</KeyValueText>
+                    <KeyValueText>
+                      <FormList>
+                        <FormListItem
+                          titleText="금리"
+                          target="#equipmentLeaseEstimateInterestRate"
+                        >
+                          <FormInvalid :error="state.interestRateError">
+                            <InputBlock :error="state.interestRateError">
+                              <InputBlockCell :flexible="true">
+                                <BasicInput
+                                  type="number"
+                                  title="적용금리"
+                                  id="equipmentLeaseEstimateInterestRate"
+                                  pattern="\d*"
+                                  :useDelete="false"
+                                  align="right"
+                                />
+                              </InputBlockCell>
+                              <template v-slot:innerRight>
+                                <div class="text-body-3">%</div>
+                              </template>
+                            </InputBlock>
+                            <FormInvalidMessage>
+                              Error Message
+                            </FormInvalidMessage>
+                          </FormInvalid>
+                        </FormListItem>
+                      </FormList>
+                    </KeyValueText>
                   </KeyValueItem>
                 </KeyValue>
               </div>
