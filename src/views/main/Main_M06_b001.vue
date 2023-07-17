0cbbb35005ae8636e6d550cb5e09ebbbf8344441
choo jeong yeon <cjy23405@mwkorea.co.kr>
2023-07-17 19:05:50 +0900

    추가 화면 파일 생성

---
+++ src/views/main/Main_M06_b001.vue
@@ -0,0 +1,57 @@
+<script>
+// Main_M06_b001
+import { ref } from 'vue';
+
+import UiLayer from '@/components/ui/layer/UiLayer.vue';
+import ToastPopup from '@/components/ui/layer/ToastPopup.vue';
+import ToastPopupHead from '@/components/ui/layer/ToastPopupHead.vue';
+import PopupTitle from '@/components/ui/layer/PopupTitle.vue';
+import ButtonList from '@/components/ui/button/ButtonList.vue';
+import ButtonListItem from '@/components/ui/button/ButtonListItem.vue';
+import BasicButton from '@/components/ui/button/BasicButton.vue';
+
+export default {
+  components: {
+    UiLayer,
+    ToastPopup,
+    ToastPopupHead,
+    PopupTitle,
+    ButtonList,
+    ButtonListItem,
+    BasicButton,
+  },
+  setup() {
+    const layer = ref(null);
+
+    return {
+      layer,
+    };
+  },
+};
+</script>
+
+<template>
+  <UiLayer ref="layer" type="toast" :backgroundClose="true">
+    <ToastPopup>
+      <template v-slot:head>
+        <ToastPopupHead>
+          <PopupTitle>타이틀</PopupTitle>
+        </ToastPopupHead>
+      </template>
+
+      // contents
+
+      <template v-slot:foot>
+        <ButtonList
+          :classNames="{
+            wrap: 'row-margin-none',
+          }"
+        >
+          <ButtonListItem>
+            <BasicButton>확인</BasicButton>
+          </ButtonListItem>
+        </ButtonList>
+      </template>
+    </ToastPopup>
+  </UiLayer>
+</template>
