7ac8367332f73d7edd513128d80ee75e09eaff3e
yoondowon <yoondw7832@mwkorea.co.kr>
2023-08-31 13:08:57 +0900

    [DT-49] Text Field Type 수정

--- src/views/uiGuide/GuideComponent.vue
+++ src/views/uiGuide/GuideComponent.vue
@@ -2763,6 +2763,7 @@ export default {
             titleText="인증번호"
             titleOptionalText="(6자리)"
             target="#testInput003"
+            :forceFocus="true"
           >
             <FormInvalid :error="state.testError001">
               <InputBlock :error="state.testError001">
@@ -2772,6 +2773,7 @@ export default {
                     pattern="\d*"
                     title="인증번호 (6자리)"
                     id="testInput003"
+                    placeholder="인증번호를 입력해 주세요"
                   />
                 </InputBlockCell>
                 <template v-slot:innerRight>
