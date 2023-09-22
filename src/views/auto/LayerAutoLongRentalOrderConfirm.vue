02b21989f691ae5fbe0fcd10387e29c9049a33f3
cheeze <cjy23405@gmail.com>
2023-09-22 14:34:20 +0900

    [DT-222] [AF_M01_l009][[AF_M01_l011] 긴급-모바일-계약조건안내, 인도요청안내 텍스트 영역 이슈

--- src/views/auto/LayerAutoLongRentalOrderConfirm.vue
+++ src/views/auto/LayerAutoLongRentalOrderConfirm.vue
@@ -124,8 +124,10 @@ export default {
           >
             <KeyValueTitle>외장색상</KeyValueTitle>
             <KeyValueText>
-              <div class="flex-box justify-conten-end">
-                <div class="flex-box__cell">아틸라스 화이트</div>
+              <div class="flex-box justify-conten-end align-items-start">
+                <div class="flex-box__cell flex-1 break-all">
+                  아틸라스 화이트
+                </div>
                 <div class="flex-box__cell">
                   <ColorChip size="small" :colors="['248, 245, 245']" />
                 </div>
@@ -140,8 +142,10 @@ export default {
           >
             <KeyValueTitle>내장색상</KeyValueTitle>
             <KeyValueText>
-              <div class="flex-box justify-conten-end">
-                <div class="flex-box__cell">네츄럴 베이지 / 다크 베이지</div>
+              <div class="flex-box justify-conten-end align-items-start">
+                <div class="flex-box__cell flex-1 break-all">
+                  네츄럴 베이지 / 다크 베이지
+                </div>
                 <div class="flex-box__cell">
                   <ColorChip
                     size="small"
@@ -158,7 +162,7 @@ export default {
             }"
           >
             <KeyValueTitle>옵션</KeyValueTitle>
-            <KeyValueText>선루프</KeyValueText>
+            <KeyValueText><div class="break-all">선루프</div></KeyValueText>
           </KeyValueItem>
 
           <KeyValueItem
