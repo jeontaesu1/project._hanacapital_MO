ef623dcc3f107116165f2e93632908d5c20245fe
yoondowon <yoondw7832@mwkorea.co.kr>
2023-10-13 11:19:00 +0900

    [DT-270]  [Common_M00_p011] 신규화면 추가

---
+++ src/views/errorContents/Common_M00_p011.vue
@@ -0,0 +1,84 @@
+<script>
+// Common_M00_p011
+import { ref, reactive } from 'vue';
+
+import UiLayer from '@/components/ui/layer/UiLayer.vue';
+import FullPopup from '@/components/ui/layer/FullPopup.vue';
+import FullPopupHead from '@/components/ui/layer/FullPopupHead.vue';
+import PopupButton from '@/components/ui/layer/PopupButton.vue';
+import ButtonList from '@/components/ui/button/ButtonList.vue';
+import ButtonListItem from '@/components/ui/button/ButtonListItem.vue';
+import BasicButton from '@/components/ui/button/BasicButton.vue';
+import IllustInfo from '@/components/ui/common/IllustInfo.vue';
+import IllustObject from '@/components/ui/common/IllustObject.vue';
+import IllustInfoTitle from '@/components/ui/common/IllustInfoTitle.vue';
+import IllustInfoText from '@/components/ui/common/IllustInfoText.vue';
+
+export default {
+  components: {
+    UiLayer,
+    FullPopup,
+    FullPopupHead,
+    PopupButton,
+    ButtonList,
+    ButtonListItem,
+    BasicButton,
+    IllustInfo,
+    IllustObject,
+    IllustInfoTitle,
+    IllustInfoText,
+  },
+  setup() {
+    const state = reactive({});
+
+    const layer = ref(null);
+
+    return {
+      state,
+      layer,
+    };
+  },
+};
+</script>
+
+<template>
+  <UiLayer ref="layer" type="full" v-slot="layerSlotProps">
+    <FullPopup>
+      <template v-slot:head>
+        <FullPopupHead>
+          <template v-slot:right>
+            <PopupButton @click="layerSlotProps.close()" />
+          </template>
+        </FullPopupHead>
+      </template>
+
+      <IllustInfo>
+        <IllustObject type="error" />
+        <IllustInfoTitle>
+          <strong>
+            원하시는 페이지를<br />
+            찾을 수 없습니다
+          </strong>
+        </IllustInfoTitle>
+        <IllustInfoText>
+          찾으려는 페이지의 주소가 잘못 입력되었거나<br />
+          주소의 변경 혹은 삭제로 사용하실 수 없습니다.<br />
+          입력하신 페이지의 주소가 정확한지<br />
+          다시 한 번 확인해 주세요.
+        </IllustInfoText>
+      </IllustInfo>
+
+      <template v-slot:foot>
+        <ButtonList
+          :classNames="{
+            wrap: 'row-margin-none',
+          }"
+        >
+          <ButtonListItem>
+            <BasicButton>홈으로 이동</BasicButton>
+          </ButtonListItem>
+        </ButtonList>
+      </template>
+    </FullPopup>
+  </UiLayer>
+</template>
