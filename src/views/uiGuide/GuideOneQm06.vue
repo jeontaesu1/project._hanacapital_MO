4bfff1cbc9573656722ad26320b53fef2fb940c6
leechaehyun <leechaehyun@mwkorea.co.kr>
2023-10-27 17:38:30 +0900

    하나 원큐 신규 화면

--- src/views/uiGuide/GuideOneQm06.vue
+++ src/views/uiGuide/GuideOneQm06.vue
@@ -7,6 +7,8 @@ import ButtonList from '@/components/ui/button/ButtonList.vue';
 import ButtonListItem from '@/components/ui/button/ButtonListItem.vue';
 
 import Q_M06_l001 from '@/views/oneQ/Q_M06_l001.vue';
+import Q_M06_l003 from '@/views/oneQ/Q_M06_l003.vue';
+import Q_M06_l002 from '@/views/oneQ/Q_M06_l002.vue';
 
 export default {
   components: {
@@ -15,17 +17,31 @@ export default {
     ButtonList,
     ButtonListItem,
     Q_M06_l001,
+    Q_M06_l003,
+    Q_M06_l002,
   },
   setup() {
     const layer001 = ref(null);
+    const layer002 = ref(null);
+    const layer003 = ref(null);
 
     const layer001Open = (e = {}) => {
       layer001.value.layer.open(e.target);
     };
+    const layer002Open = (e = {}) => {
+      layer002.value.layer.open(e.target);
+    };
+    const layer003Open = (e = {}) => {
+      layer003.value.layer.open(e.target);
+    };
 
     return {
       layer001,
+      layer002,
+      layer003,
       layer001Open,
+      layer002Open,
+      layer003Open,
     };
   },
 };
@@ -44,8 +60,20 @@ export default {
           >아파트 시세 검색<br />Q_M06_l001</BasicButton
         >
       </ButtonListItem>
+      <ButtonListItem>
+        <BasicButton @click="layer002Open"
+          >아파트 등기부등본 팝업<br />Q_M06_l002</BasicButton
+        >
+      </ButtonListItem>
+      <ButtonListItem>
+        <BasicButton @click="layer003Open"
+          >등기부등본 확인 팝업<br />Q_M06_l003</BasicButton
+        >
+      </ButtonListItem>
     </ButtonList>
 
     <Q_M06_l001 ref="layer001" />
+    <Q_M06_l002 ref="layer002" />
+    <Q_M06_l003 ref="layer003" />
   </PageContents>
 </template>
