9c00122de60620530ad7406deece5bfe674bc443
leechaehyun <leechaehyun@mwkorea.co.kr>
2023-12-20 14:45:40 +0900

    23.12.20 배포

--- src/views/customer/CustomerNoticeDetail.vue
+++ src/views/customer/CustomerNoticeDetail.vue
@@ -8,6 +8,9 @@ import PageContents from '@/components/ui/layout/PageContents.vue';
 import BasicButton from '@/components/ui/button/BasicButton.vue';
 import ButtonList from '@/components/ui/button/ButtonList.vue';
 import ButtonListItem from '@/components/ui/button/ButtonListItem.vue';
+import TextButton from '@/components/ui/button/TextButton.vue';
+import IconRightArrow from '@/assets/images/icon/right-arrow.svg?component';
+import BasicHr from '@/components/ui/common/BasicHr.vue';
 
 export default {
   components: {
@@ -15,6 +18,9 @@ export default {
     BasicButton,
     ButtonList,
     ButtonListItem,
+    TextButton,
+    IconRightArrow,
+    BasicHr,
   },
   setup() {
     const store = {
@@ -52,6 +58,175 @@ export default {
       </section>
     </div>
 
+    <!-- Case : 버튼 1개일 경우 -->
+    <ButtonList>
+      <ButtonListItem>
+        <BasicButton to="/customer/notice-list">버튼</BasicButton>
+      </ButtonListItem>
+    </ButtonList>
+    <!-- // Case : 버튼 1개일 경우 -->
+
+    <!-- Case : 버튼 2개일 경우 -->
+    <ButtonList>
+      <ButtonListItem>
+        <BasicButton to="/customer/notice-list">버튼</BasicButton>
+      </ButtonListItem>
+      <ButtonListItem>
+        <BasicButton to="/customer/notice-list">버튼</BasicButton>
+      </ButtonListItem>
+    </ButtonList>
+    <!-- // Case : 버튼 2개일 경우 -->
+
+    <!-- Case : 버튼 3개일 경우 -->
+    <ButtonList>
+      <ButtonListItem>
+        <BasicButton to="/customer/notice-list">버튼</BasicButton>
+      </ButtonListItem>
+      <ButtonListItem>
+        <BasicButton to="/customer/notice-list">버튼</BasicButton>
+      </ButtonListItem>
+      <ButtonListItem>
+        <BasicButton to="/customer/notice-list">버튼</BasicButton>
+      </ButtonListItem>
+    </ButtonList>
+    <!-- // Case : 버튼 3개일 경우 -->
+
+    <ButtonList align="full">
+      <!-- Case : 버튼 컬러 타입 #143B75 -->
+      <ButtonListItem>
+        <BasicButton theme="tertiary">버튼</BasicButton>
+      </ButtonListItem>
+      <!-- // Case : 버튼 컬러 타입 #143B75 -->
+
+      <!-- Case : 버튼 컬러 타입 #143B75  -->
+      <ButtonListItem>
+        <BasicButton theme="secondary">버튼</BasicButton>
+      </ButtonListItem>
+      <!-- // Case : 버튼 컬러 타입 #143B75 -->
+
+      <!-- Case : 버튼 컬러 타입 #EAEAEA  -->
+      <ButtonListItem>
+        <BasicButton theme="quaternary">버튼</BasicButton>
+      </ButtonListItem>
+      <!-- // Case : 버튼 컬러 타입 #EAEAEA  -->
+
+      <!-- Case : 버튼 텍스트 컬러 타입 #00A69E  -->
+      <ButtonListItem>
+        <BasicButton theme="quaternary-green">버튼</BasicButton>
+      </ButtonListItem>
+      <!-- // Case : 버튼 텍스트 컬러 타입 #00A69E  -->
+
+      <!-- Case : 버튼 텍스트 컬러 타입 #3A81E0  -->
+      <ButtonListItem>
+        <BasicButton theme="quaternary-blue">버튼</BasicButton>
+      </ButtonListItem>
+      <!-- // Case : 버튼 텍스트 컬러 타입 #3A81E0  -->
+
+      <!-- Case : 버튼 텍스트 컬러 타입 #00A69E  -->
+      <ButtonListItem>
+        <BasicButton :line="true" theme="quaternary-green">버튼</BasicButton>
+      </ButtonListItem>
+      <!-- Case : 버튼 텍스트 컬러 타입 #00A69E  -->
+
+      <!-- Case : 버튼 텍스트 컬러 타입 #3A81E0  -->
+      <ButtonListItem>
+        <BasicButton :line="true" theme="quaternary-blue">버튼</BasicButton>
+      </ButtonListItem>
+      <!-- // Case : 버튼 텍스트 컬러 타입 #3A81E0  -->
+    </ButtonList>
+
+    <!-- Case : 버튼 1개일 경우 -->
+    <ButtonList>
+      <TextButton theme="secondary" :iconFillAll="true">
+        <template v-slot:rightIcon>
+          <IconRightArrow />
+        </template>
+        버튼
+      </TextButton>
+    </ButtonList>
+    <!-- // Case : 버튼 1개일 경우 -->
+
+    <!-- Case : 버튼 2개일 경우 -->
+    <ButtonList>
+      <ButtonListItem>
+        <TextButton theme="secondary" :iconFillAll="true">
+          <template v-slot:rightIcon>
+            <IconRightArrow />
+          </template>
+          버튼
+        </TextButton>
+      </ButtonListItem>
+      <ButtonListItem>
+        <TextButton theme="secondary" :iconFillAll="true">
+          <template v-slot:rightIcon>
+            <IconRightArrow />
+          </template>
+          버튼
+        </TextButton>
+      </ButtonListItem>
+    </ButtonList>
+    <!-- // Case : 버튼 2개일 경우 -->
+
+    <!-- Case : 버튼 3개일 경우 -->
+    <ButtonList>
+      <ButtonListItem>
+        <TextButton theme="secondary" :iconFillAll="true">
+          <template v-slot:rightIcon>
+            <IconRightArrow />
+          </template>
+          버튼
+        </TextButton>
+      </ButtonListItem>
+      <ButtonListItem>
+        <TextButton theme="secondary" :iconFillAll="true">
+          <template v-slot:rightIcon>
+            <IconRightArrow />
+          </template>
+          버튼
+        </TextButton>
+      </ButtonListItem>
+      <ButtonListItem>
+        <TextButton theme="secondary" :iconFillAll="true">
+          <template v-slot:rightIcon>
+            <IconRightArrow />
+          </template>
+          버튼
+        </TextButton>
+      </ButtonListItem>
+    </ButtonList>
+    <!-- // Case : 버튼 3개일 경우 -->
+
+    <!-- Case : 버튼 컬러 타입 #3A81E0  -->
+    <ButtonList>
+      <TextButton theme="tertiary" :iconFillAll="true">
+        <template v-slot:rightIcon>
+          <IconRightArrow />
+        </template>
+        버튼
+      </TextButton>
+    </ButtonList>
+    <!-- // Case : 버튼 컬러 타입 #3A81E0  -->
+
+    <!-- Case : 버튼 컬러 타입 #757575  -->
+    <ButtonList>
+      <TextButton class="color-gray" :iconFillAll="true">
+        <template v-slot:rightIcon>
+          <IconRightArrow />
+        </template>
+        버튼
+      </TextButton>
+    </ButtonList>
+    <!-- // Case : 버튼 컬러 타입 #757575  -->
+
+    <!-- DD : 버튼 추가시 노출 -->
+    <BasicHr
+      type="contents"
+      theme="quaternary"
+      className="row-margin-contents row-margin-bottom-none"
+    />
+    <!-- DD : 버튼 추가시 노출 -->
+
+    <!-- Case: 상시 노출 -->
     <ButtonList>
       <ButtonListItem>
         <BasicButton
@@ -64,6 +239,7 @@ export default {
         </BasicButton>
       </ButtonListItem>
     </ButtonList>
+    <!-- // Case: 상시 노출 -->
   </PageContents>
 </template>
 
